4d58b1767526aa623cd8bffa8f8796534390a7f1
Adrian Valenzuela <avalenzuela85@gmail.com>
2021-09-11 22:40:30 -0700

    remove gold logo hover effect with alpine because it was buggy

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -33,13 +33,10 @@ Alpine.start()
 // sticky header
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myStickyHeader()};
-
 // Get the header
 var header = document.getElementById("myHeader");
-
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
-
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myStickyHeader() {
   if (window.pageYOffset > sticky) {
