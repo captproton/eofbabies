b5f22e88e4125cceadfa0bbb5365be6199b9479f
Adrian Valenzuela <avalenzuela85@gmail.com>
2021-09-22 00:29:06 -0700

    added swupjs

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -75,24 +75,21 @@ lightGallery(document.getElementById('lightgallery'), {
 });
 
 // swup js
-//import Swup from 'swup';
-//const swup = new Swup();
-/*
+import Swup from 'swup';
+const swup = new Swup();
+
 document.addEventListener('swup:contentReplaced', (event) => {
 	// scroll to top
 	window.scrollTo(0, 0);
 	// home banner display none
-	if (document.getElementById('home-banner')) {
+	if (!!document.getElementById('home-banner')) {
 		var homeBanner = document.getElementById('home-banner');
 		homeBanner.classList.add('hidden');
-	}
+	} else {
+		homeBanner.classlist.remove('hidden');
+	};
   // do something when content is replaced
 });
-*/
-
-//import Highway from '@dogstudio/highway';
-//const H = new Highway.Core();
-
 
 // custom css file to override css from javascript plugins
 import "custom.css"
